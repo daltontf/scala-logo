767ae8394f88c7dc4e9b187c88c57beb63c8c13d
Tim Dalton <daltont@ociweb.com>
2009-07-01 21:19:50 -0500

    Made enhancements

--- src/main/scala/tfd/logo/Logo.scala
+++ src/main/scala/tfd/logo/Logo.scala
@@ -1,23 +1,15 @@
-package logo
+package tfd.logo
 
-import javax.swing.{AbstractAction, ImageIcon, JComponent, JPanel, JFrame, JSplitPane, JTextArea, JToolBar}
+import javax.swing.{AbstractAction, ImageIcon, JButton, JComponent, JPanel, JFileChooser, JFrame, JOptionPane, JScrollPane, JSplitPane, JTextArea, JToolBar, SwingWorker}
+import javax.swing.filechooser.{FileNameExtensionFilter}
 import java.awt.{BorderLayout, Color, Dimension, Graphics, Graphics2D}
 import java.awt.event.{ActionEvent}
 import java.awt.image.{BufferedImage}
+import java.io.{BufferedReader, BufferedWriter, File, FileReader, FileWriter}
 
 import tfd.gui.swing.CutCopyPastePopupSupport
 
-import scala.util.parsing.combinator.syntactical.StandardTokenParsers
-
-object GraphicUtils {
-  
-  def doto[T](target: T)(block:T => Any):T = {
-        block(target)
-        target
-  }
-}
-
-import GraphicUtils._
+import scala.util.parsing.combinator.JavaTokenParsers
 
 abstract class LogoOp
 case class Home() extends LogoOp
@@ -28,102 +20,188 @@ case class Repeat(i: Int, e: List[LogoOp]) extends LogoOp
 case class Position(x:Int, y:Int)
 
 class DrawCanvas extends JComponent {
-  private val image = new BufferedImage(350, 350, BufferedImage.TYPE_3BYTE_BGR)
-  
-  private val mySize = new Dimension(350,350)
-    			  
-  setSize(mySize)
-  setMinimumSize(mySize)
-  setMaximumSize(mySize)
-  
-  def doDraw(logoCode: String) {
+  private var image:BufferedImage = null
+  private var imageHeight:Int = 0
+  private var imageWidth:Int = 0
+  private var parseResult:LogoParser.ParseResult[List[LogoOp]] = null
+
+  override def setSize(size:Dimension) {
+	super.setSize(size)
+	if (size.height > 0 && size.width > 0) {
+	  imageHeight = size.height
+	  imageWidth = size.width
+	  if (image != null) image.flush()
+   	  image = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_3BYTE_BGR)
+      if (parseResult != null) {
+        doDraw()
+      }
+	} else {
+	  image = null
+	}
+  }
+ 
+  def updateParseResult(parseResult: LogoParser.ParseResult[List[LogoOp]]) {
+    this.parseResult = parseResult
+    doDraw()
+  }
+    
+  def doDraw() {
       val g = image.getGraphics.asInstanceOf[Graphics2D]
 	  g.setColor(Color.WHITE)
-      g.fillRect(0, 0, 350, 350)
+      g.fillRect(0, 0, imageWidth, imageHeight)
       g.setColor(Color.BLACK)
-      g.translate(175, 175)
-      Logo.evaluate(logoCode, g);
+      g.translate(imageWidth/2, imageHeight/2)
+      if (parseResult != null) (new Logo).evaluate(parseResult, g)
       repaint()
   }
   
   override def paintComponent(g: Graphics)  = {
-    g.drawImage(image, 0, 0, this)
+    if (image != null) g.drawImage(image, 0, 0, this)
   }
 }
 
 class MainApplication {
-	var points:List[(Int,Int)] = _
+	var currentFile:File = _
+  
+	val canvas = new DrawCanvas
     
-    val canvas = new DrawCanvas
+    val codeText = new JTextArea();
+    { 	import codeText._
+    	setToolTipText("Enter Logo code here")
+        CutCopyPastePopupSupport.enable(codeText)
+    }
     
-    val codeText = new JTextArea
-    codeText setText """REPEAT 18 [ RT 20 REPEAT 9 [ FD 40 RT 40]]"""
-    CutCopyPastePopupSupport.enable(codeText)
-
-    val consoleText = new JTextArea
-    consoleText.setPreferredSize(new Dimension(700, 120))
-    consoleText.setEditable(false)
-    CutCopyPastePopupSupport.enable(consoleText)
-
-	doto(new JFrame()) { $ =>
-    	doto($.getContentPane) { $ =>
-    	  	$.add(doto (new JToolBar()) { $ =>
-    	  	    $.setFloatable(false)
-    	  	  	$.add(new AbstractAction("New", new ImageIcon(getClass().getClassLoader().getResource("./new.png"))) {
-    	  		  
-    	  				def actionPerformed(ae:ActionEvent) {
-    	  					
-    	  				}
-    	  		  
-    	  			})  
-    	  		$.add(new AbstractAction("Open", new ImageIcon(getClass().getClassLoader().getResource("./open.png"))) {
-    	  		  
-    	  				def actionPerformed(ae:ActionEvent) {
-    	  					
-    	  				}
-    	  		  
-    	  			})  
-                $.add(new AbstractAction("Save", new ImageIcon(getClass().getClassLoader().getResource("./save.png"))) {
-    	  		  
-    	  				def actionPerformed(ae:ActionEvent) {
-    	  					
-    	  				}
-    	  		  
-    	  			})  
-    	  		$.add(new AbstractAction("Run", new ImageIcon(getClass().getClassLoader().getResource("./go.png"))) {
-    	  		  
-    	  				def actionPerformed(ae:ActionEvent) {
-    	  					System.out.println(LogoParser.parse(codeText.getText))
-    	  					canvas.doDraw(codeText.getText)
-    	  					canvas.repaint();
-    	  				}
-    	  		  
-    	  			})    	  	  
-              }, BorderLayout.NORTH)
-    		$.add(
-    			new JSplitPane(
-    				JSplitPane.VERTICAL_SPLIT,
-    					doto(
-    						new JSplitPane(
-    						JSplitPane.HORIZONTAL_SPLIT,
-    						codeText,
-    						canvas
-    					)) { $ => 
-    					  $.setDividerLocation(350)
-    					  $.setPreferredSize(new Dimension(720, 350))
-    					}, consoleText
-    				)
-    		      ,BorderLayout.CENTER)
-    	}
-    	$.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-        $.pack
-    	$.setVisible(true)
+    val consoleText = new JTextArea();
+    {   import consoleText._
+    	setPreferredSize(new Dimension(700, 120))
+    	setEditable(false)
+    	setToolTipText("Console for Logo Parser")
+    	CutCopyPastePopupSupport.enable(consoleText)
+    }
+    
+    def icon(s:String) = new ImageIcon(getClass().getClassLoader().getResource(s))
+    
+    lazy val newAction = new AbstractAction {
+					override def actionPerformed(ae:ActionEvent) {
+						codeText.setText("")
+					}
      }
-}
+    
+    lazy val openAction = new AbstractAction {
+    				override def actionPerformed(ae:ActionEvent) {
+    					val chooser = new JFileChooser
+    					chooser.setFileFilter(new FileNameExtensionFilter("Logo Files", "logo"))
+    					if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
+    						val file = chooser.getSelectedFile
+    						if (file.exists) {
+    							new SwingWorker[StringBuffer,Unit]() {
+    								override def doInBackground():StringBuffer = {
+    										val reader = new BufferedReader(new FileReader(file))
+    										val buffer = new StringBuffer(file.length.asInstanceOf[Int])
+    										while (reader.ready) {
+    											buffer.append(reader.readLine)
+    										}
+    										reader.close
+    										buffer
+    									}
+    							    
+    								override def done() {
+    									codeText.setText(get().toString)
+    								}
+    							}.execute
+    						}
+    					}
+    				}
+    			}
+    
+    lazy val saveAction = new AbstractAction() {
+      		import javax.swing.JOptionPane._
+            
+    		override def actionPerformed(ae:ActionEvent) {
+    			val chooser = new JFileChooser
+    			chooser.setFileFilter(new FileNameExtensionFilter("Logo Files", "logo"))
+    			if (chooser.showSaveDialog(frame) == JFileChooser.APPROVE_OPTION) {
+    				val selectedFile = chooser.getSelectedFile
+    				val file = if (selectedFile.getName.indexOf(".") >= 0) {
+    					chooser.getSelectedFile
+    				} else {
+    					new File(selectedFile.getAbsolutePath + ".logo")
+    				}
+    				if (file.exists() &&
+    					JOptionPane.showConfirmDialog(frame, "File Exists", "Existing file will be overwritten", OK_CANCEL_OPTION) != OK_OPTION) {
+    					return
+    				}
+  					new SwingWorker[Unit,Unit]() {
+  						override def doInBackground() {
+  							val writer = new BufferedWriter(new FileWriter(file))
+   	 								val text = codeText.getText
+   	 								writer.write(text, 0, text.length)
+   	 								writer.close
+   	 							}
+   	 						}.execute
+      			}
+    	}
+    }
+    
+    lazy val runAction = new AbstractAction {
+    	  		  
+  			override def actionPerformed(ae:ActionEvent) {
+   				new SwingWorker[LogoParser.ParseResult[List[LogoOp]],Unit]() {
+   	  					   	  				    
+   					override def doInBackground() = LogoParser.parse(codeText.getText)
+   	  				  	
+   					override def done() {    	  				  	
+   						val parseResult = get 
+   						consoleText.append(parseResult.toString)
+   						consoleText.append("\n")
+   						canvas.updateParseResult(parseResult)
+   					}
+   	  			}.execute    	  		  
+   	  		}
+     }    
 
+	val frame = new JFrame("Scala Logo")
+ 	val contentPane = frame.getContentPane
+	val toolBar = new JToolBar
+    toolBar.setFloatable(false)
+ 
+    { 
+      import toolBar._
+      
+      def createButtonForAction(action:AbstractAction, iconFile:String, toolTipText:String) = {
+    	  val btn = new JButton(action)
+    	  	btn.setIcon(icon(iconFile))
+      		btn.setToolTipText(toolTipText)
+      		btn
+      }
+		add(createButtonForAction(newAction, "./new.png", "Start new Logo program"))
+    	add(createButtonForAction(openAction, "./open.png", "Open existing .logo file"))
+    	add(createButtonForAction(saveAction, "./save.png", "Save current logo code to file"))
+    	add(createButtonForAction(runAction, "./go.png", "Run Logo program"))
+    }
+    
+    val codeCanvasPane = new JSplitPane(
+    		JSplitPane.HORIZONTAL_SPLIT,
+    		new JScrollPane(codeText),
+    		new JScrollPane(canvas)
+    )
+    codeCanvasPane.setDividerLocation(350)
+    codeCanvasPane.setPreferredSize(new Dimension(720, 350))
+    contentPane.add(toolBar, BorderLayout.NORTH) 
+    contentPane.add(new JSplitPane(
+    	JSplitPane.VERTICAL_SPLIT,
+    		codeCanvasPane,
+    		new JScrollPane(consoleText)
+    	),BorderLayout.CENTER)
+    
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+    frame.pack
+    frame.setVisible(true)
+   }
 
-
-object Logo {
+class Logo {
+  var heading = 0
+  var position = Position(0,0)
   
   implicit def dblToInt(d: Double): Int = 
     if (d  > 0 ) (d +0.5).toInt 
@@ -134,34 +212,54 @@ object Logo {
  
   def parse(s: String) : List[LogoOp] = LogoParser.parse(s).get
   
-  def evaluate(s : String, g:Graphics2D):Unit = evaluate(parse(s), g, Position(0,0), 0); 
-
-  def evaluate(e : List[LogoOp], g:Graphics2D, position: Position, heading: Int):Unit =  
+  def evaluate(parseResult: LogoParser.ParseResult[List[LogoOp]], g:Graphics2D) {
+    if (parseResult.successful) {
+      evaluate(parseResult.get, g)
+    }
+    // draw turtle
+    evaluate(parse("RT 90 FD 3 LT 110 FD 10 LT 140 FD 10 LT 110 FD 3"), g)
+  }
+  
+  def evaluate(e : List[LogoOp], g:Graphics2D):Unit =  
     e match {
+    
     case Nil => Nil
-    case Home() :: tail => evaluate(tail, g, Position(0,0),0)
+    
+    case Home() :: tail => {
+      heading = 0
+      position = Position(0,0)
+      evaluate(tail, g)
+    }
+                            
     case Forward(x) :: tail => {
       val nextPosition = forward(position, x, heading)
-      g.drawLine(position.x, position.y, nextPosition.x, nextPosition.y) 
-      evaluate(tail, g, nextPosition, heading)
+      g.drawLine(position.x, position.y, nextPosition.x, nextPosition.y)
+      position = nextPosition
+      evaluate(tail, g)
     }
-    case Turn(x) :: tail => evaluate(tail, g, position, heading + x)
-    case Repeat(0, y) :: tail => evaluate(tail, g, position, heading)
-    case Repeat(x, y) :: tail => evaluate(y ::: Repeat(x-1, y)::tail, g, position, heading)
+    
+    case Turn(x) :: tail => {
+      heading += x
+      evaluate(tail, g)
+    }
+    
+    case Repeat(0, y) :: tail => evaluate(tail, g)
+    
+    case Repeat(x, y) :: tail => evaluate(y ::: Repeat(x-1, y)::tail, g)
+    
     case _ => Nil
   }
 }
    
-  object LogoParser extends scala.util.parsing.combinator.JavaTokenParsers  {
-    
-      
+  object LogoParser extends JavaTokenParsers  {
+          
     def program:Parser[List[LogoOp]] = rep(stmt)
     
     def forward = ("FD"|"FORWARD")~>wholeNumber ^^ { case value => Forward(value.toInt) }
     
-    def right = ("RT"|"RIGHT")~>wholeNumber ^^ { case value => Turn(value.toInt) }
+    def right = ("RT"|"RIGHT")~>wholeNumber ^^ { case value => Turn(-value.toInt) }
     
-    def left = ("LT"|"LEFT")~>wholeNumber ^^ { case value => Turn(-value.toInt) }
+    def left = ("LT"|"LEFT")~>wholeNumber ^^ { case value => Turn(value.toInt) }
                                                
     def repeat = "REPEAT" ~> wholeNumber ~ "[" ~ rep(stmt) ~ "]" ^^ { case number~_~stmts~_ => Repeat(number.toInt, stmts) }
         
---
+++ src/test/scala/tfd/logo/LogoTest.scala
@@ -0,0 +1,144 @@
+package tfd.logo
+
+import org.junit._
+import org.junit.Assert._
+
+import java.awt.{Component, Container}
+
+import javax.swing._
+
+import java.io.{FileInputStream, IOException}
+import java.util.concurrent.{Executors}
+
+import org.netbeans.jemmy.{ComponentChooser, JemmyProperties, QueueTool}
+import org.netbeans.jemmy.operators._
+
+class LogoTest {
+	lazy val launchExecutor = Executors.newSingleThreadExecutor();
+
+	@Before
+	def setUp() {
+		JemmyProperties.setCurrentTimeout("ComponentOperator.WaitComponentTimeout", 2000)
+	}
+
+//	@After
+//	def tearDown() {
+//		var break = false
+//		while (!break) {
+//			try {
+//				val frame = JFrameOperator.findJFrame(new JFrameOperator.JFrameFinder())
+//				if (frame != null) {
+//					new JFrameOperator(frame).close()
+//				} else {
+//					break
+//				}
+//			} catch {
+//			case _ => break = true
+//			}
+//		}
+//		queueWait(1000)
+//	}
+ 
+	def queueWait(max: Long) {
+		val qt = new QueueTool()
+		val to = qt.getTimeouts().cloneThis()
+		to.setTimeout("QueueTool.WaitQueueEmptyTimeout", 2000)
+		qt.setTimeouts(to)
+		qt.waitEmpty(10)
+	}
+ 
+	def launchMain() {
+//		launchExecutor.execute(new Runnable() {
+//			
+//			def run() {
+				new MainApplication 
+//			}
+//	
+//		})
+	}
+ 
+	class LoanChooser[A <: java.awt.Component](clazz: Class[A], filter: A => Boolean) extends ComponentChooser {
+		override def checkComponent(component:Component) = 
+			if (component.getClass.isAssignableFrom(clazz)) {
+				filter(component.asInstanceOf[A])
+			} else {
+				false
+			}
+		  
+		override def getDescription() = "Generic Finder using Loan Pattern"
+	}
+
+	def findJFrame(filter: JFrame => Boolean) = 
+		new JFrameOperator(JFrameOperator.findJFrame(new LoanChooser(classOf[JFrame], filter)))
+ 
+	def findJButton(container:Container, filter: JButton => Boolean) = {
+		val button = JButtonOperator.findJButton(container, new LoanChooser(classOf[JButton], filter))
+		if (button == null) {
+		  fail("cannot find JButton with matching criteria")
+		}
+        new JButtonOperator(button)
+    }   
+     
+//    def findJButton(container:Container, toolTipText:String) = {
+//		val button = JButtonOperator.findJButton(container, 
+//			new ComponentChooser() {
+//				override def checkComponent(component:Component) = 
+//					if (component.getClass.isAssignableFrom(classOf[JButton])) {
+//						component.asInstanceOf[JButton].getToolTipText == toolTipText
+//					} else {
+//						false
+//					}
+//		  
+//				override def getDescription() = "JButton Finder using toolTipText"
+//			})
+//		if (button == null) {
+//		  fail("cannot find JButton with tooltip = '" + toolTipText + "'")
+//		}
+//		new JButtonOperator(button)
+//    }
+	
+	def findJTextArea(frmOper:JFrameOperator, filter: JTextArea => Boolean) = 
+		new JTextAreaOperator(JTextAreaOperator.findJTextArea(frmOper.getContentPane, new LoanChooser(classOf[JTextArea], filter)))
+ 
+	def toolTipTextEquals(toolTipText:String)(component:JComponent) = component.getToolTipText == toolTipText
+ 
+	implicit def jFrameOperator2Container(frmOper:JFrameOperator) = frmOper.getContentPane
+ 
+	@Test
+	def testMain() = {
+		launchMain()
+		queueWait(2000)
+		
+		val frmOper = findJFrame(frm => frm.getTitle == "Scala Logo")
+  
+//		val contentPane = frmOper.getContentPane
+//		val newButtonOper = findJButton(frmOper, toolTipTextEquals("Start new Logo program"))
+//		val openButtonOper = findJButton(frmOper, toolTipTextEquals("Open existing .logo file"))
+//		val saveButtonOper = findJButton(frmOper, toolTipTextEquals("Save current logo code to file"))
+//		val runButtonOper = findJButton(frmOper, toolTipTextEquals("Run Logo program"))
+        
+		val List(newButtonOper, openButtonOper, saveButtonOper, runButtonOper) = 
+            List("Start new Logo program",
+                 "Open existing .logo file",
+                 "Save current logo code to file",
+        		 "Run Logo program").map(toolTipText => findJButton(frmOper, toolTipTextEquals(toolTipText)))
+		
+        val List(codeOper, consoleOper) =
+			List("Enter Logo code here",
+				 "Console for Logo Parser").map(toolTipText => findJTextArea(frmOper, toolTipTextEquals(toolTipText)))
+		
+		def runLogo(code:String, parserOutput:String) = {
+			codeOper.setText("")
+			consoleOper.setText("")
+			codeOper.setText(code)
+			runButtonOper.clickMouse()
+			Thread.sleep(1000)
+			assertEquals(parserOutput, consoleOper.getText)
+		}
+  
+		runLogo("", "[1.1] parsed: List()\n")
+		runLogo("x", "[1.1] failure: string matching regex `\\z' expected but `x' found\n\nx\n^\n")
+		runLogo("FORWARD 10", "[1.11] parsed: List(Forward(10))\n")
+		runLogo("FD 10", "[1.6] parsed: List(Forward(10))\n")
+	}	
+}
